e3883188f91c17fefa2fd9b22ef20a85e51d2fc8
Luis Davi <d7a8v9i@gmail.com>
2023-06-18 02:09:33 -0300

    capitulo 1

--- src/App.java
+++ src/App.java
@@ -1,25 +1,64 @@
 import java.util.Scanner;
 
 public class App {
+    
     public static void main(String[] args){
          Scanner insira = new Scanner(System.in);
 
          //introdução
 
-         digitacao("- Riqueza ",50);
+         digitacao("  Riquezas ",50);
          System.out.println("\n");
 
-         digitacao("- Fama",50);
+         digitacao("  Fama",50);
          System.out.println("\n");
 
-         digitacao("- Poder",50);
+         digitacao("  Poder",50);
          System.out.println("\n");
 
-         digitacao("- O homem que conseguiu tudo neste mundo, o Rei dos Piratas, Gold Roger.\n", 50);
-         digitacao("Suas últimas palavras antes de ser executado, foram enviadas para todos no mar.\n",50);
-         digitacao("- Gold roger  'Minhas riquezas e tesouros? Se vocês quiserem, eu os deixo pegar.'\n",50);
-         digitacao("e assim se iniciou a grande era dos piratas",50);
+         digitacao("  Um homem conquistou tudo que o mundo tinha a oferecer, o Rei dos Piratas, Gold Roger."+
+         "\nAntes de ser executado, suas últimas palavras levaram multidões aos mares: \n",40);
+         System.out.println("");
+         digitacao("- Gold roger ''Querem o meu tesouro? Fiquem a vontade para pegá-lo, procurem, nele está tudo que este mundo pode dar a vocês!''\n",50);
+         System.out.println("");
+         digitacao("E assim se iniciou a grande era dos piratas.",50);
     
+         //Nome
+         System.out.println("\n");
+         digitacao("Olá Aventureiro(a), como posso te chamar ? ",50);
+         String nome = insira.nextLine();
+         
+         //Capitulo I
+         System.out.println("");
+         digitacao("Capitulo I | A Escolha do Destino\n",70);
+         System.out.println("");
+
+         digitacao("No Nort Blue, um(a) jovem chamado(a) "+nome+" se vê em um dilema."+
+         "\nCriado(a) em uma vila pacífica, ele(a) sonha em se tornar um marinheiro(a) para proteger os outros. "+
+         "\nMas a atração pelo desconhecido e a promessa de liberdade o(a) tentam a se juntar a um bando pirata.\n"+
+         "Entre a responsabilidade e a aventura, "+nome+" precisa escolher seu caminho!!", 40); 
+        
+         //primeira escolha
+         System.out.println("\n"+"\n");
+
+         digitacao("| Pirata | Em busca de tesouros e liberdade, embarque em aventuras\n"+
+         "perigosas pelos mares desconhecidos, desafiando a ordem estabelecida e\n"+
+         "enfrentando rivais implacáveis. O destino do One Piece aguarda por você!!", 30);
+
+         System.out.println("\n"+"\n");
+
+         digitacao("| Marinheiro | Defenda a justiça e a paz nos mares como um membro da Marinha.\n"+
+         "Enfrente piratas perigosos, proteja os inocentes e lute contra a corrupção,\n"+
+         "garantindo um mundo seguro para todos. Sua missão é a ordem e a proteção!!", 30);
+
+         String escolha = insira.nextLine();
+
+         if(escolha.equalsIgnoreCase("piratas")){
+             digitacao("", 40);
+         }
+         else{
+
+         }
 
 
 
