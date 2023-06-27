0ac75cd20a77e951df5ab7d52b2dc8a27e97fcc3
Luis Davi <d7a8v9i@gmail.com>
2023-06-27 20:45:42 -0300

    sistema de energia com 1 método

--- src/personagem.java
+++ src/personagem.java
@@ -12,43 +12,35 @@ public class personagem{
         this.nome = nome;
     }     
  
-    public String Nome(){
-        return nome;
-    }
-    
-    public void perdeBravura(int quantidade){
-        bravura = bravura - quantidade;
-    
-        if(bravura > 0){
+    public void mudaBravura(int quantidade){
+        bravura = bravura += quantidade;
+        
+        if(bravura > 0 && quantidade<0){
             System.out.println(nome);
-            System.out.println("Perdeu "+quantidade+" de bravura" );
+            System.out.println(quantidade+" de bravura" );
             System.out.println("Sua bravura atual é de: "+bravura);
             System.out.println("");
         }
 
-        else{
+        else if(bravura<0){
             System.out.println(nome);
-            System.out.println("Perdeu "+quantidade+" de bravura" );
+            System.out.println(quantidade+" de bravura" );
             System.out.println("Sua bravura se esgotou.");
             System.out.println(nome+" Não consegirá alcançar o One Piece...");
             System.out.println("");
            }
 
-    }
-
-    public void ganhaBravura(int quantidade){
-        bravura = bravura + quantidade;
         
-        if(bravura < 190){
+        else if(bravura < 190 && quantidade>0){
             System.out.println(nome);
-            System.out.println("Ganhou "+quantidade+" de bravura" );
+            System.out.println(quantidade+" de bravura" );
             System.out.println("Sua bravura atual é de: "+bravura);
             System.out.println("");
         }
 
         else{
             System.out.println(nome);
-            System.out.println("Ganhou "+quantidade+" de bravura" );
+            System.out.println(quantidade+" de bravura" );
             System.out.println("Sua bravura chegou ao Maximo!!");
             System.out.println(nome+" Conquistou seu sonho !!");
             System.out.println("");
