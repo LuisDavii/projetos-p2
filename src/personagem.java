4b0138b7d9230606f167434cd2c3b4f3268c97f7
Luis Davi <d7a8v9i@gmail.com>
2023-06-27 22:02:57 -0300

    correção do sistema

--- src/personagem.java
+++ src/personagem.java
@@ -17,14 +17,14 @@ public class personagem{
         
         if(bravura > 0 && quantidade<0){
             System.out.println(nome);
-            System.out.println(quantidade+" de bravura" );
+            System.out.println("Perdeu "+quantidade+" de bravura" );
             System.out.println("Sua bravura atual é de: "+bravura);
             System.out.println("");
         }
 
         else if(bravura<0){
             System.out.println(nome);
-            System.out.println(quantidade+" de bravura" );
+            System.out.println("Perdeu "+quantidade+" de bravura" );
             System.out.println("Sua bravura se esgotou.");
             System.out.println(nome+" Não consegirá alcançar o One Piece...");
             System.out.println("");
@@ -33,14 +33,14 @@ public class personagem{
         
         else if(bravura < 190 && quantidade>0){
             System.out.println(nome);
-            System.out.println(quantidade+" de bravura" );
+            System.out.println("Ganhou "+quantidade+" de bravura" );
             System.out.println("Sua bravura atual é de: "+bravura);
             System.out.println("");
         }
 
         else{
             System.out.println(nome);
-            System.out.println(quantidade+" de bravura" );
+            System.out.println("Ganhou "+quantidade+" de bravura" );
             System.out.println("Sua bravura chegou ao Maximo!!");
             System.out.println(nome+" Conquistou seu sonho !!");
             System.out.println("");
