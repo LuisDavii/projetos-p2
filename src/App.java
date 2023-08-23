555cb4a6f8c33ded03593ae31e6bda57e0be8bad
Luis Davi <d7a8v9i@gmail.com>
2023-08-22 22:24:37 -0300

    atualização na historia

--- src/App.java
+++ src/App.java
@@ -5,8 +5,7 @@ import java.util.HashMap;
 public class App {
         static leitorDeArquivos leitor = new leitorDeArquivos();
         public static void main(String[] args) {
-                Scanner insira = new Scanner(System.in);
-        
+
                 // introdução
                 print("  Riquezas ");
                 print("  Fama");
@@ -16,13 +15,11 @@ public class App {
                 print("- Gold roger ''Querem o meu tesouro? Fiquem a vontade para pegá-lo, procurem, nele está tudo que este mundo pode dar a vocês!''");
                 print("E assim se iniciou a grande era dos piratas.");
 
-                print("Olá Aventureiro(a), como posso te chamar ? ");
-                String principal = insira.nextLine();
                 HashMap<String,personagem> personagens = leitor.lerPersonagens("rsc/personagens.txt");
                 HashMap<Integer,capitulo> capitulos = leitor.lerCap("rsc/historiaCap.txt",personagens);
-                personagens.put("principal", new personagem(principal, 100));
 
                 capitulos.get(1).executa();
+
         }                                       
         public static void print(String texto) {
                 capitulo.print(texto, 20);
